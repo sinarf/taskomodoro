0de5fab052ee8d65721318fd86a0176df4904628
Michel Blavin <michel@blavin.fr>
2020-03-18 16:51:42 +0100

    Added hello world module

---
+++ src/taskomodoro/taskomodoro.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# main entry point of the module
+
+
+def main():
+    """
+    main entry point
+    """
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
